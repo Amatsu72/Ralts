1010dda139b32aa785b438bfffe4b61516d83fe7
wangshanqiang <wangshanqiang1@xiaohongshu.com>
2024-11-13 16:36:37 +0800

    feature:11/13，引入glad，glm，stb_image

--- Ralts/src/main.cpp
+++ Ralts/src/main.cpp
@@ -1,7 +1,8 @@
 #include "App/Application.h"
+#include <glm/glm.hpp>
 
 int main(int argc, char** argv) {
     std::cout << "Hello, world!" << std::endl;
-    std::cout << "This is a test program!!!!" << std::endl;
+    std::cout << "This is a test program!!!222" << std::endl;
     return 0;
 }
